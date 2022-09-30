9102b6518c7362e137d0318be8b0b3ca8728e1e6
Miguel Young de la Sota <mcyoung@mit.edu>
2022-09-30 19:42:12 -0400

    Smack everything with the formatter

--- bad/source/lib.rs
+++ bad/source/lib.rs
@@ -1,50 +1,52 @@
 pub mod bad {
-	pub enum ProgramSource {
-		Path(std::ffi::OsString),
-		File(std::fs::File),
-		Stdin(std::io::Stdin),
-	}
-	
-	pub struct CompilationConfiguration {
-		pub sources: std::vec::Vec<ProgramSource>
-	}
+  pub enum ProgramSource {
+    Path(std::ffi::OsString),
+    File(std::fs::File),
+    Stdin(std::io::Stdin),
+  }
 
-	#[derive(Debug)]
-	pub enum Token {
-		LeftParen,
-		RightParen,
-		LeftBrace,
-		RightBrace,
-		Semicolon,
-		SingleQuote,
-		Codepoint(char),
-		Identifier(std::string::String)
+  pub struct CompilationConfiguration {
+    pub sources: std::vec::Vec<ProgramSource>,
+  }
 
-	}
+  #[derive(Debug)]
+  pub enum Token {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Semicolon,
+    SingleQuote,
+    Codepoint(char),
+    Identifier(std::string::String),
+  }
 
-	#[derive(Debug)]
-	pub struct TokenList {
-		pub tokens : std::vec::Vec<Token>
-	}
+  #[derive(Debug)]
+  pub struct TokenList {
+    pub tokens: std::vec::Vec<Token>,
+  }
 
-	#[derive(Debug)]
-	pub struct SyntaxTree {
+  #[derive(Debug)]
+  pub struct SyntaxTree {}
 
-	}
+  pub fn lex(_config: &CompilationConfiguration) -> TokenList {
+    let list: TokenList = TokenList {
+      tokens: std::vec::Vec::new(),
+    };
+    list
+  }
 
-	pub fn lex (_config: &CompilationConfiguration) -> TokenList {
-		let list: TokenList = TokenList { tokens: std::vec::Vec::new() };
-		list
-	}
+  pub fn parse(
+    token_stream: TokenList,
+    _config: &CompilationConfiguration,
+  ) -> SyntaxTree {
+    let tree = SyntaxTree {};
+    tree
+  }
 
-	pub fn parse (token_stream: TokenList, _config: &CompilationConfiguration) -> SyntaxTree {
-		let tree = SyntaxTree {  };
-		tree
-	}
-	
-	pub fn compile (config: &CompilationConfiguration) -> SyntaxTree {
-		let lex: TokenList = lex(&config);
-		let tree: SyntaxTree = parse(lex, &config);
-		tree
-	}	
+  pub fn compile(config: &CompilationConfiguration) -> SyntaxTree {
+    let lex: TokenList = lex(&config);
+    let tree: SyntaxTree = parse(lex, &config);
+    tree
+  }
 }
