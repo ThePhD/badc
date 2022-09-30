58ad2523d8bb7b9fd24574097ad7beade5d62a37
Miguel Young de la Sota <mcyoung@mit.edu>
2022-09-30 19:42:12 -0400

    Smack everything with the formatter

--- badc/source/main.rs
+++ badc/source/main.rs
@@ -1,14 +1,21 @@
 pub mod badc {
-	pub struct CompilationOptions {
-		pub sources: std::vec::Vec<bad::bad::ProgramSource>,
-	}
+  pub struct CompilationOptions {
+    pub sources: std::vec::Vec<bad::bad::ProgramSource>,
+  }
 }
 
-fn main () -> () {
-	// TODO: parse from Command Line and/or JSON
-	let options: badc::CompilationOptions = badc::CompilationOptions{ sources :vec!(bad::bad::ProgramSource::Path(std::ffi::OsString::from("./main.b"))) };
-	// TODO: properly transfer compilation options to library configuration
-	let config : bad::bad::CompilationConfiguration = bad::bad::CompilationConfiguration{ sources: options.sources };
-	let tree : bad::bad::SyntaxTree = bad::bad::compile(&config);
-	println!("{:?} 🎉!", tree);
+fn main() -> () {
+  // TODO: parse from Command Line and/or JSON
+  let options: badc::CompilationOptions = badc::CompilationOptions {
+    sources: vec![bad::bad::ProgramSource::Path(std::ffi::OsString::from(
+      "./main.b",
+    ))],
+  };
+  // TODO: properly transfer compilation options to library configuration
+  let config: bad::bad::CompilationConfiguration =
+    bad::bad::CompilationConfiguration {
+      sources: options.sources,
+    };
+  let tree: bad::bad::SyntaxTree = bad::bad::compile(&config);
+  println!("{:?} 🎉!", tree);
 }
