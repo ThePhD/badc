75f74d3dbdf7e21c0de6e357628f9537d5e4d607
ThePhD <phdofthehouse@gmail.com>
2022-11-03 15:30:16 -0400

    🎨 Format available files to get proper, approved structure.

--- badc/source/main.rs
+++ badc/source/main.rs
@@ -1,9 +1,13 @@
-use std::{path::PathBuf};
 use clap::Parser;
+use std::path::PathBuf;
 
 /// The badc compiler for the B language (Kernighan, 1969). Learning experiment for DrawsMiguel and ThePhD on Rust and some compilation techniques. Released un the CC0 1.0 Universal (e.g. Public Domain dedication).
 #[derive(Parser, Debug)]
-#[command(author, version, about = "A B language (Kernighan, 1969) compiler. Not at all useful.")]
+#[command(
+	author,
+	version,
+	about = "A B language (Kernighan, 1969) compiler. Not at all useful."
+)]
 struct CommandLineCompilationOptions {
 	/// All of the paths to the input to compile, each one considered an independent translation unit.
 	inputs: Vec<PathBuf>,
@@ -37,17 +41,29 @@ fn main() {
 	for input in args.inputs {
 		let output = match args.output {
 			Some(ref target_path) => target_path.clone(),
-			None => { let mut target_path = input.clone(); target_path.push(".out"); target_path },
+			None => {
+				let mut target_path = input.clone();
+				target_path.push(".out");
+				target_path
+			}
 		};
 		let print_tokens_output = match args.print_tokens_output {
 			Some(ref target_path) => target_path.clone(),
-			None => { let mut target_path = output.clone(); target_path.push(".badc_tokens"); target_path },
+			None => {
+				let mut target_path = output.clone();
+				target_path.push(".badc_tokens");
+				target_path
+			}
 		};
 		let print_ast_output = match args.print_ast_output {
 			Some(ref target_path) => target_path.clone(),
-			None => { let mut target_path = output.clone(); target_path.push(".badc_ast"); target_path },
+			None => {
+				let mut target_path = output.clone();
+				target_path.push(".badc_ast");
+				target_path
+			}
 		};
-		let config = bad::CompilationConfiguration{
+		let config = bad::CompilationConfiguration {
 			input: bad::ProgramSource::Path(input.clone()),
 			print_tokens: args.print_tokens,
 			print_ast: args.print_ast,
